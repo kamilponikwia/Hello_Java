b77c0871113c7975b842d86b2bc6657224a2386d
Kamil Ponikwia <kamil.ponikwia@gmail.com>
2022-06-06 08:52:56 +0200

    new printf

--- Hello.java
+++ Hello.java
@@ -6,5 +6,6 @@ public class Hello {
 
    public static void main(String []args) {
       System.out.println("Hello World!!!"); // prints Hello World!
+      System.out.println("test branch!!!");
    }
 }
